6b542de8e02518ae23f8ca614a58c165e35b116a
viktorbarzin <vbarzin@gmail.com>
2019-05-23 17:23:58 +0100

    remove hard to use guestfs and utilize some bash commands instead

--- lansync.py
+++ lansync.py
@@ -1,5 +1,5 @@
 import os
-import guestfs
+# import guestfs
 from utilities import ArgParser, get_pub_keys, is_key_imported, import_key, get_local_ip, parse_size, get_first_partition_offset
 from settings import CURRENT_USER, PATH_TO_PUBLIC_DIR, AUTHORIZED_KEYS_PATH, PATH_TO_PUBLIC_DIR_FILE, PUBLIC_DIR_FILE_NAME
 
@@ -37,26 +37,44 @@ def parse_import_key(key_to_import: str) -> None:
         import_key(key_to_import, AUTHORIZED_KEYS_PATH)
         print(f'Imported key successfully: {key_to_import[:40]}...<trimmed>...{key_to_import[-20:]}')
 
+###############################################################################
+# guestfs module is dodgy to install so commented out for now
+###############################################################################
+# def create_share(share_name, share_size='10M') -> str:
+#     output = os.path.join(PATH_TO_PUBLIC_DIR_FILE, share_name)
+#     g = guestfs.GuestFS(python_return_dict=True)
+#     # g.set_trace(1)
+
+#     parsed_share_size = parse_size(share_size)
+#     if parsed_share_size is None:
+#         raise ValueError(f'Invalid share size: {share_size}')
+#     g.disk_create(output, 'raw', parsed_share_size)
+#     g.add_drive_opts(output, format="raw", readonly=0)
+#     g.launch()
+#     devices = g.list_devices()
+#     assert(len(devices) == 1)
+#     g.part_disk(devices[0], 'mbr')
+#     partitions = g.list_partitions()
+#     assert(len(partitions) == 1)
+#     g.mkfs('fat', partitions[0])
+#     g.close()
+#     return share_name
+
 
 def create_share(share_name, share_size='10M') -> str:
-    output = os.path.join(PATH_TO_PUBLIC_DIR_FILE, share_name)
-    g = guestfs.GuestFS(python_return_dict=True)
-    # g.set_trace(1)
-
+    '''
+    Utilize some commonly found shell commands to create the share file
+    '''
     parsed_share_size = parse_size(share_size)
-    if parsed_share_size is None:
-        raise ValueError(f'Invalid share size: {share_size}')
-    g.disk_create(output, 'raw', parsed_share_size)
-    g.add_drive_opts(output, format="raw", readonly=0)
-    g.launch()
-    devices = g.list_devices()
-    assert(len(devices) == 1)
-    g.part_disk(devices[0], 'mbr')
-    partitions = g.list_partitions()
-    assert(len(partitions) == 1)
-    g.mkfs('fat', partitions[0])
-    g.close()
-    return share_name
+    output = os.path.join(PATH_TO_PUBLIC_DIR_FILE, share_name)
+
+    # create share file
+    fallocate_cmd = 'fallocate -l ' + str(parsed_share_size) + ' ' + output
+    os.system(fallocate_cmd)
+
+    # create partition on the file
+    os.system('mkfs.fat ' + output)
+    return output
 
 
 if __name__ == "__main__":
